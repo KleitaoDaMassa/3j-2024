7ed6274be25a8be6b6abd3a03b5718da557cba6b
Vinisim reis <108934754+KleitaoDaMassa@users.noreply.github.com>
2024-06-20 07:56:41 -0300

    aula dia 20/06

--- main.js
+++ main.js
@@ -6,10 +6,22 @@ const tempoObjetivo1 = new Date (2025, 03, 15, 15, 00);
 const tempoObjetivo2 = new Date (2027, 11, 29, 23, 00);
 const tempoObjetivo3 = new Date (2025, 03, 16, 30, 00);
 const tempoObjetivo4 = new Date (2027, 06, 04, 17, 45);
-tempo[0].textContent=tempoObjetivo1
-tempo[1].textContent=tempoObjetivo2
-tempo[2].textContent=tempoObjetivo3
-tempo[3].textContent=tempoObjetivo4
+const agora = new Date() 
+let segundos;
+let minutos;
+let horas;
+let dias;
+
+segundos (tempoObjetivo1-agorạ/1000)
+minutos = segundos/60
+horas = minutos/60
+dias = horas/24
+
+
+
+tempo[0].textContent= tempoObjetivo1-agora
+tempo[1].textContent= textContent = agora
+
 
 console.log(tempo);
 for(let i = 0; 1 < botoes.length ; i++){
